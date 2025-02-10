c36af7291a40e25ad2973908c60bfffe12ada5b2
NICHOLAS YURI DE SOUZA ROSA <nicholas.ysrosa@senacsp.edu.br>
2025-02-10 14:28:04 -0300

    aula

--- app.js
+++ app.js
@@ -15,6 +15,23 @@ app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem vindo a nossa API! " });
 });
 
+// Criação de usuario
+app.post("/auth/register", async (req, res) => {
+  const { name, email, password, confirmpassword } = req.body
+
+  if (!name) {
+    return res.status(422).json({ msg: "O nome é obrigatório" });
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatório" });
+  }
+});
+
 // Credenciais
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
